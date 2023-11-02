5f484e959caa5d7924d0024f00140a5232f1f2d1
anovikov <anovikovpost@gmail.com>
2023-11-02 08:55:28 +0300

    added modem_status and modemup endpoints

--- API.py
+++ API.py
@@ -56,6 +56,10 @@ REDIS_HOST = os.environ['REDIS_HOST']
 REDIS_PORT = int(os.environ['REDIS_PORT'])
 REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
 
+# REDIS_HOST = 'localhost'
+# REDIS_PORT = 6379
+# REDIS_PASSWORD = 'Lyq5Rad8'
+
 ALLOWED_PROTOCOLS = ['http', 'socks', 'both']
 
 MYSQL_SETTINGS = {
@@ -1733,6 +1737,117 @@ class ProxyCount(Resource):
             if connection:
                 connection.close()
 
+class ModemStatus(Resource):
+    @requires_role("admin")
+    def post(self, admin_token):
+        try:
+            logging.info("Received request to CHECK MODEM STATUS.")
+
+            data = request.json
+            if data is None:
+                return {"message": "Invalid request: JSON body required"}, 400
+            
+            token = data.get('token')
+
+            user_data = get_data_from_redis(token)
+            serial_number = user_data.get('serial')
+            device_model = user_data.get('device')
+            mode = user_data.get('mode')
+            
+            if not serial_number:
+                logging.error("Serial number not found in user data.")
+                return {'error': 'Serial number not found'}, 400
+
+            handler = MODEM_HANDLERS.get(device_model, {}).get('status')
+            if not handler:
+                logging.error("Invalid device model provided. Use a correct 'device' field.")
+                return {"message": "Invalid device model provided. Use a correct 'device' field."}, 400
+
+            status = handler(serial_number) if handler else None
+
+            if status == "device_not_found":
+                logging.error("Device not found, possibly it has lost connection")
+                return {"message": "Device not found, possibly it has lost connection"}, 500
+            elif status == "timeout":
+                logging.error("Device timed out, possibly it has lost connection")
+                return {"message": "Device timed out, possibly it has lost connection"}, 500
+
+            logging.info(f"Modem status for serial {serial_number}: {status}")
+            return {"message": status}, 200
+
+        except Exception as e:
+            logging.error(f"An error occurred: {str(e)}")
+            return {"message": f"An error occurred: {str(e)}"}, 500
+
+class ModemUp(Resource):
+    @requires_role("admin")
+    def post(self, admin_token):
+        try:
+            logging.info("Received request to SWITCH MODEM.")
+
+            data = request.json
+            if data is None:
+                return {"message": "Invalid request: JSON body required"}, 400
+            
+            token = data.get('token')
+
+            user_data = get_data_from_redis(token)
+            serial_number = user_data.get('serial')
+            device_model = user_data.get('device')
+            mode = user_data.get('mode')
+            id = user_data.get('id')
+            interface_name = f'id{id}'
+
+            logging.info(f"SWITCHING to {mode}: id{id}, type {device_model}, serial {serial_number}")
+
+            if not all([serial_number, device_model, mode]):
+                return {"message": "Missing required fields"}, 400
+
+            status_handler = MODEM_HANDLERS.get(device_model, {}).get('status')
+            status = status_handler(serial_number) if status_handler else None
+
+            if status == "device_not_found":
+                logging.error("Device not found, possibly it has lost connection")
+                return {"message": "Device not found, possibly it has lost connection"}, 500
+            elif status == "timeout":
+                logging.error("Device timed out, possibly it has lost connection")
+                return {"message": "Device timed out, possibly it has lost connection"}, 500
+
+            if mode == "modem":
+                if status == "rndis":
+                    ip_address = wait_for_ip(interface_name)
+                    if ip_address != '127.0.0.1':
+                        logging.info("Modem is already on")
+                        return {"message": "Modem is already on", "ip_address": ip_address}, 200
+                    logging.error("Interface not ready, unable to get IP address")
+                    return {"message": "Interface not ready, unable to get IP address"}, 500
+                else:
+                    handler = MODEM_HANDLERS.get(device_model, {}).get('on')
+                    handler(serial_number)
+                    ip_address = wait_for_ip(interface_name)
+                    if ip_address != '127.0.0.1':
+                        logging.info("Modem turned on successfully")
+                        return {"message": "Modem turned on successfully", "ip_address": ip_address}, 200
+                    logging.error("Interface not ready, unable to get IP address")
+                    return {"message": "Interface not ready, unable to get IP address"}, 500
+
+            elif mode == "android":
+                if status == "rndis":
+                    handler = MODEM_HANDLERS.get(device_model, {}).get('off')
+                    handler(serial_number)
+                    logging.info("Modem turned off successfully")
+                    return {"message": "Modem turned off successfully"}, 200
+                else:
+                    logging.info("Modem is already turned off")
+                    return {"message": "Modem is already turned off"}, 200
+            else:
+                logging.error("Invalid mode provided. Use either 'modem' or 'parent' as mode field.")
+                return {"message": "Invalid mode provided. Use either 'modem' or 'parent' as mode field."}, 400
+
+        except Exception as e:
+            logging.error(f"An error occurred: {str(e)}")
+            return {"message": "Internal server error"}, 500
+
 #resources
 api.add_resource(Reboot, '/api/reboot/<string:token>') #user role
 api.add_resource(DeviceStatus, '/api/device_status/<string:token>') #user role
@@ -1745,8 +1860,8 @@ api.add_resource(UpdateMode, '/api/update_mode/<string:token>') #admin role
 
 api.add_resource(UpdateUser, '/api/update_user/<string:token>') #admin role
 api.add_resource(ChangeDevice, '/api/change_device/<string:token>') #admin role
-#api.add_resource(ModemToggle, '/api/modem/<string:token>') #admin role
-# don't useapi.add_resource(ModemStatus, '/api/modemstatus/<string:token>/<string:device_model>') #admin role
+api.add_resource(ModemUp, '/api/modemup/<string:token>') #admin role
+api.add_resource(ModemStatus, '/api/modemstatus/<string:token>') #admin role
 api.add_resource(ProxyCount, '/api/proxycount/<string:token>') #admin role
 
 if __name__ == '__main__':
